2f79ab9251b6ce3dde2fda8c8324264c4dc85218
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-08-15 12:50:37 +0530

    slow&fast-pointer --> lc_876. Middle of the Linked List

---
+++ slowAndFast-pointer/lc_876.cpp
@@ -0,0 +1,15 @@
+//876. Middle of the Linked List
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
