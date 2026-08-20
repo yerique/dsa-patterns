8b6fcdeed65b24eb75d16231657a71f8a4c72f1d
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-08-20 18:07:11 +0530

    gfg_Overlapping Intervals

---
+++ merge-intervals/gfg_checkOverlappingIntervals.cpp
@@ -0,0 +1,22 @@
+//gfg_Overlapping Intervals
+
+class Solution {
+public:
+    bool isIntersect(vector<vector<int>> intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        int s1 = intervals[0][0];
+        int e1 = intervals[0][1];
+        for(int i = 1; i < n; i++) {
+            int s2 = intervals[i][0];
+            int e2 = intervals[i][1];
+            if(e1 >= s2){
+                return true;
+            }
+            s1 = s1;
+            e1 = max(e1, e2);
+        }
+        return false;
+
+    }
+};
