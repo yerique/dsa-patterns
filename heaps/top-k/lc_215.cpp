cdcf682fabc98230d3a51aaad692a6be7291cef7
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-08-30 18:11:20 +0530

    heaps(top-k) --> lc_215. Kth Largest Element in an Array

---
+++ heaps/top-k/lc_215.cpp
@@ -0,0 +1,21 @@
+//215. Kth Largest Element in an Array
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0; i < k; i++){
+            pq.push(nums[i]);
+        }
+        for(int i = k; i < n; i++){
+            if(pq.top() >= nums[i]){
+                continue;
+            }else{
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};
