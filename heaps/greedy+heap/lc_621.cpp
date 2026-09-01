f82e7e89a0bc1ac3f16a0ac99e6ce93466bb3d5d
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-09-01 19:33:14 +0530

    heap(greedy+heap)--> more intutive soln of lc_621

--- heaps/greedy+heap/lc_621.cpp
+++ heaps/greedy+heap/lc_621.cpp
@@ -1,5 +1,6 @@
 //621. Task Scheduler
 
+//one-way to do is this-a better in the below
 struct cmp {
     bool operator()(const pair<int, char>&a, const pair<int, char>&b) const{
         if(a.first != b.first) return (a.first < b.first);
@@ -103,3 +104,50 @@ public:
  *                      +-----------------------------+
  * ============================================================================
  */
+
+//a better solution:
+struct cmp {
+    bool operator()(const pair<int, char>&a, const pair<int, char>&b) const{
+        if(a.first != b.first) return (a.first < b.first);
+        return (a.second < b.second);
+    }
+};
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int m = tasks.size();
+        unordered_map<char, int> frequency;
+        for (int i = 0; i < m; i++) {
+            frequency[tasks[i]]++;
+        }
+        priority_queue<pair<int, char>, vector<pair<int, char>>, cmp> pq;
+        unordered_map<char, int> eligiblePos;
+        for (auto task : frequency) {
+            pq.push({task.second, task.first});
+            eligiblePos[task.first] = 1;
+        }
+        int currTaskPos = 1;
+        while (!pq.empty()) {
+            vector<pair<int, char>> temp;
+            while (!pq.empty()) {
+                pair<int, char > maxTask = pq.top();
+                pq.pop();
+                if (currTaskPos >= eligiblePos[maxTask.second]) {
+                    maxTask.first--;
+                    if (maxTask.first > 0) {
+                        pq.push(maxTask);
+                    }
+                    eligiblePos[maxTask.second] = currTaskPos + n + 1;
+                    break;
+                }else {
+                    temp.push_back(maxTask);
+                }
+            }
+            for (auto task : temp) {
+                pq.push(task);
+            }
+            currTaskPos++;
+        }
+        return currTaskPos - 1;
+    }
+};
