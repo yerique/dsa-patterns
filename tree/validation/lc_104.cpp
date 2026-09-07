84fe86a0eed7c82713b20b696a0d8abe871d4763
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-09-07 20:17:06 +0530

    tree(validation) --> lc_104. Maximum Depth of Binary Tree

---
+++ tree/validation/lc_104.cpp
@@ -0,0 +1,13 @@
+//104. Maximum Depth of Binary Tree
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1 + max(left, right);
+    }
+};
