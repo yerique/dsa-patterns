b2278683c23ec5a2e0a15f206da06ad96222d4b9
Sudhanshu Nag <sudhanshunag99@gmail.com>
2026-09-16 02:50:01 +0530

    dp -> fibonacci done with tabulations

--- dp/lc_509(via-dp).cpp
+++ dp/lc_509(via-dp).cpp
@@ -14,3 +14,35 @@ public:
         return ans;
     }
 };
+
+//via dp tabulation:
+ class Solution {
+ public:
+     int fib(int n) {
+         if(n == 0 || n == 1) return n;
+         vector<int> dp(n+1);
+         dp[0] = 0;
+         dp[1] = 1;
+         for(int i = 2; i <= n; i++){
+             dp[i] = dp[i-1] + dp[i-2];
+         }
+         return dp[n];
+     }
+ };
+
+//dp tabulation with optimized space
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1) return n;
+        int prevPrev = 0;
+        int prev = 1;
+        int ans = 0;
+        for(int i = 2; i <=n; i++){
+            ans = prev + prevPrev;
+            prevPrev = prev;
+            prev = ans;
+        }
+        return ans;
+    }
+};
